c1ace92149123845d2877c72c5ca4970e1e52e58
AKIBRAt <aleksej.rodic11@gmail.com>
2024-12-07 11:53:24 +0100

    porukica

---
+++ punoletan.py
@@ -0,0 +1,10 @@
+try:
+    godineeeeeeeeeeeeeee=int(input("unesite godine"))
+    if godineeeeeeeeeeeeeee<0 or godineeeeeeeeeeeeeee>120:
+        print("pogresan unos")
+    elif godineeeeeeeeeeeeeee<=18:
+        print("maloletan si")
+    elif godineeeeeeeeeeeeeee>18:
+        print("punoletan si")
+except:
+    print("e mislim da malo  nisi fer")
